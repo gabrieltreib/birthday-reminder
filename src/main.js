b1b027d0823f639b30da63289a2ab7958fb1642a
Gabriel Treib <treib159@gmail.com>
2021-07-25 17:36:40 -0300

    ajustando cabeçalho n° aniversarios

--- src/main.js
+++ src/main.js
@@ -4,10 +4,9 @@ import { data } from './data'
 const BirthdayReminder = () => {
  const [people, setPeople] = React.useState(data);
 
-
-
  return (
   <>
+   <h1>{people.length} birthdays today</h1>
    {people.map((person) => {
     const { id, name, age, img } = person;
 
@@ -15,7 +14,7 @@ const BirthdayReminder = () => {
      <div key={id} className='item'>
       <h4>{name}</h4>
       <h4>{age}</h4>
-      <img src={img}></img>
+      <img src={img} alt=''></img>
      </div>
 
     );
